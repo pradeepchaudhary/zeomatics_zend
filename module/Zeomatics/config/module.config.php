b9781e61fb95c12ac15e83cd0a90a88a52288ec5
Pradeep Chaudhary <pradeepchaudhary.iitr@gmail.com>
2014-09-24 15:06:36 +0530

    second commit

--- module/Zeomatics/config/module.config.php
+++ module/Zeomatics/config/module.config.php
@@ -99,6 +99,8 @@ return array(
         //Overriding the default layout
         'template_map' => array(
         'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+        'error/404' => __DIR__ . '/../view/error/404.phtml',
+        'error/index' => __DIR__ . '/../view/error/index.phtml',
         ),
     ),
 );
