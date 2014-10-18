ff8d72a77e0d7bd93d18b110213928c87f27d0a8
Pradeep Chaudhary <pradeepchaudhary.iitr@gmail.com>
2014-10-18 20:59:52 +0530

    minor changes

--- public/js/jquery.bxslider.js
+++ public/js/jquery.bxslider.js
@@ -207,7 +207,7 @@
 			slider.viewport.css({
 				width: '100%',
 				overflow: 'hidden',
-				position: 'relative'
+//				position: 'relative'
 			});
 			slider.viewport.parent().css({
 				maxWidth: getViewportMaxWidth()
